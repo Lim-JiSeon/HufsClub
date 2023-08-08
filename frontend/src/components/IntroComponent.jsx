a93e0716314b2f55e466df2311f9e661ddc97a26
Lim_JiSeon <dpf226@naver.com>
2023-08-08 17:17:08 +0900

    [feat]create_pages

---
+++ frontend/src/components/IntroComponent.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styled from "styled-components";
+
+function IntroComponent() {
+  return <></>;
+}
+
+export default IntroComponent;
---
+++ frontend/src/components/MainComponent.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styled from "styled-components";
+
+function MainComponent() {
+  return <></>;
+}
+
+export default MainComponent;
