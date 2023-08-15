3823bf01964c5184c019c9dc6d1c88f7a85b09d4
Lim_JiSeon <dpf226@naver.com>
2023-08-15 20:46:57 +0900

    [FEAT] Login page 구현 완료

---
+++ frontend/src/components/Input.js
@@ -0,0 +1,18 @@
+import styled from "@emotion/styled";
+
+const Input = styled.input`
+  display: block;
+  width: 100%;
+  padding: 4px 6px;
+  height: 48px;
+  border-radius: 4px;
+  font-size: 14px;
+  border: 1px solid #333;
+  background-color: white;
+  text-align: center;
+  box-sizing: border-box;
+  outline: none;
+  margin-top: 16px;
+`;
+
+export default Input;
--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import LoginForm from "../components/LoginForm";
 
 function LoginPage() {
-  return <>LoginPage</>;
+  return (
+    <div style={{height: "100vh", display: "flex", justifyContent: "center", alignItems: "center"}}>
+      <LoginForm />
+    </div>
+  );
 }
 
 export default LoginPage;
