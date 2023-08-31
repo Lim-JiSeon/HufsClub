bbf76565ca217fc3d953e56def049e5fe9edf47e
Lim_JiSeon <dpf226@naver.com>
2023-08-31 16:36:39 +0900

    pr 에러 수정중

--- frontend/src/components/func/Input.js
+++ frontend/src/components/func/Input.js
@@ -1,18 +1,38 @@
 import styled from "@emotion/styled";
 
-const Input = styled.input`
+const Wrapper = styled.div`
+  display: block;
+  margin-top: 16px;
+`;
+
+const Label = styled.label`
+  display: block;
+  font-size: 14px;
+  font-weight: bold;
+  margin-bottom: 4px;
+  padding-left: 8px;
+`;
+
+const StyledInput = styled.input`
   display: block;
   width: 100%;
   padding: 4px 6px;
   height: 48px;
   border-radius: 4px;
-  font-size: 14px;
+  font-size: 16px;
   border: 1px solid #333;
   background-color: white;
-  text-align: center;
   box-sizing: border-box;
   outline: none;
-  margin-top: 16px;
 `;
+
+const Input = ({ label, ...props }) => {
+  return (
+    <Wrapper>
+      <Label>{label}</Label>
+      <StyledInput {...props} />
+    </Wrapper>
+  );
+};
 
 export default Input;
