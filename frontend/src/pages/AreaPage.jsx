fb613ff8dce2482a6064bf39523188d285574fdb
Lim_JiSeon <dpf226@naver.com>
2023-09-13 18:51:45 +0900

    [FEAT] 검색페이지 이미지 배너 추가 및 분야별 변수 정의

--- frontend/src/pages/AreaPage.jsx
+++ frontend/src/pages/AreaPage.jsx
@@ -1,7 +1,43 @@
+import styled from "@emotion/styled";
 import React from "react";
+import { useParams } from "react-router-dom";
+import Header from "../components/Header";
+import Image from "../constants/Image";
 
 function AreaPage() {
-  return <>AreaPage</>;
+  const area = useParams().field;
+
+  const AreaContainer = styled.div`
+    display: flex;
+    width: 100%;
+    height: 100%;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  `;
+
+  const Banner = styled.img`
+    width: 90vw;
+    height: 300px;
+    border-radius: 10px;
+    border: none;
+  `;
+
+  return (
+    <AreaContainer>
+      <Header></Header>
+      <Banner src={getImgURL(area)} />
+    </AreaContainer>
+  );
 }
 
 export default AreaPage;
+
+export const getImgURL = (area) => {
+  if (area === "학술") return Image.ACADEMIC;
+  if (area === "종교") return Image.RELIGION;
+  if (area === "스포츠") return Image.SPORTS;
+  if (area === "친목") return Image.AMITY;
+  if (area === "문화") return Image.CULTURE;
+  if (area === "봉사") return Image.VOLUNTEER;
+};
