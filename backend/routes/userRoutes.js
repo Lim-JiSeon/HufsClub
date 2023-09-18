b1d3da4aedf846e9df7d5b592fe308da1677b069
yewonshin <rac8793@naver.com>
2023-09-18 23:48:35 +0900

    이메일 전송 테스트용 코드 작성

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -169,6 +169,24 @@ userRouter.put(
     }
   })
 );
+
+/* 이메일 전송 테스트용 코드
+const formData = require('form-data');
+const Mailgun = require('mailgun.js');
+const mailgun = new Mailgun(formData);
+const mg = mailgun.client({username: 'api', key: process.env.MAILGUN_API_KEY || 'pubkey-372c004bef6d74e2d3f33ba028326f95'});
+
+mg.messages.create('sandbox-123.mailgun.org', {
+	from: "Excited User <mailgun@sandbox-123.mailgun.org>",
+	to: ["test@example.com"],
+	subject: "Hello",
+	text: "Testing some Mailgun awesomeness!",
+	html: "<h1>Testing some Mailgun awesomeness!</h1>"
+})
+.then(msg => console.log(msg)) // logs response data
+.catch(err => console.log(err)); // logs any error
+*/
+
 /*
 userRouter.post(
   '/forget-password',
