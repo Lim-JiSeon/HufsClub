42488c6def090127d77908c82dfbfe9bda4ef994
Lim_JiSeon <dpf226@naver.com>
2023-10-04 20:13:54 +0900

    [FIX] 데이터 처리 에러 수정

--- frontend/src/api/search.js
+++ frontend/src/api/search.js
@@ -1,16 +1,8 @@
 import axios from "axios";
 
-const search = (keyword) => {
+const search = async (keyword) => {
   const API_END_POINT = "http://localhost:5000/api/";
-  axios
-    .get(`${API_END_POINT}clubs/search?keyword=${keyword}`)
-    .then((res) => {
-      console.log(res.data)
-      return res.data;
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+  return axios.get(`${API_END_POINT}clubs/search?keyword=${keyword}`);
 };
 
 export default search;
