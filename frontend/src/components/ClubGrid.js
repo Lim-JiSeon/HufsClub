22ebf5d7658f194f9f5f593ffffae4538b5609a2
Lim_JiSeon <dpf226@naver.com>
2023-11-15 09:21:45 +0900

    feat: 검색 페이지 구현 완료

--- frontend/src/components/ClubGrid.js
+++ frontend/src/components/ClubGrid.js
@@ -8,14 +8,21 @@ const GridWrap = styled.div`
   border-radius: 20px;
   border: solid 2px #27374d;
   margin-bottom: 20px;
+  padding: 30px;
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  place-items: center;
+  gap: 10px;
 `;
 
 const ClubGrid = (data) => {
   const clubs = data.data.data;
-  console.log(clubs);
+ 
   return (
     <GridWrap>
-      {clubs && clubs.map((club) => <ClubItem data={club} />)}
+      {clubs
+        ? clubs.map((club) => <ClubItem key={club._id} data={club} />)
+        : null}
     </GridWrap>
   );
 };
