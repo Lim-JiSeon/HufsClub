8d9de31c7baec26b7463fa8289bea7fc52a3567d
Lim_JiSeon <dpf226@naver.com>
2024-01-11 13:10:09 +0900

    fix: 에러 이슈 해결

--- frontend/src/api/findId.js
+++ frontend/src/api/findId.js
@@ -2,15 +2,20 @@ import axios from "axios";
 
 const findId = (name, email) => {
   const API_END_POINT = "http://localhost:5000/api/";
-
-  try {
-    axios.post(`${API_END_POINT}users/forget-id`, {
-      username: name,
-      email: email,
-    });
-    alert("입력하신 이메일로 인증번호를 보냈습니다.");
-  } catch (error) {
-    alert("회원을 찾을 수 없습니다.");
+  if (name && email) {
+    axios
+      .post(`${API_END_POINT}users/forget-id`, {
+        username: name,
+        email: email,
+      })
+      .then(() => {
+        alert("입력하신 이메일로 인증번호를 보냈습니다.");
+      })
+      .catch(() => {
+        alert("회원을 찾을 수 없습니다.");
+      });
+  } else {
+    alert("이름과 이메일을 모두 입력해주세요.");
   }
 };
 
